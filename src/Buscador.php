70113d3f499e3750ca1ec5018797113112738cb8
Luiz Moura <lfmoura@gmail.com>
2022-09-30 17:46:47 -0300

    Complete application.

---
+++ src/Buscador.php
@@ -0,0 +1,30 @@
+<?php
+namespace LuizMoura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador{
+    protected $httpClient;
+    private Crawler $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url) : array{
+        $resposta = $this->httpClient->request('GET', $url);
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos =  $this->crawler->filter(selector: 'span.card-curso__nome');
+        $cursos = [];
+        foreach ($elementosCursos as $elemento){
+            $cursos [] = $elemento->textContent;
+        }
+        return $cursos;
+    }
+
+}
